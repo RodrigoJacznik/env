4acc39aa3434a34b2c5283547c65cb892c145006
Rodrigo Jacznik <jacznik.rodrigo@gmail.com>
2025-10-19 02:13:43 -0300

    fix missing packages. Enables ly service

--- scripts/02-i3.sh
+++ scripts/02-i3.sh
@@ -2,10 +2,13 @@
 set -euo pipefail
 
 sudo pacman -S --noconfirm --needed \
-	i3-wm xorg xorg-init \
+	i3-wm \
+	xorg-server \
+	xorg-xinit \
 	picom \
 	rofi \
 	polybar \
 	dunst \
 	ly
 
+sudo systemctl enable ly.service
