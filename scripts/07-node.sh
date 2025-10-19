10140d30f8e44b7a02117644669876b9a541039f
Rodrigo Jacznik <jacznik.rodrigo@gmail.com>
2025-10-19 02:33:28 -0300

    Add npm-global to path

--- scripts/07-node.sh
+++ scripts/07-node.sh
@@ -3,8 +3,12 @@ set -euo pipefail
 
 sudo pacman -S --noconfirm --needed nodejs 
 
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
+export NPM_GLOBAL_DIR="$HOME/.npm-global"
+
+mkdir -p "$NPM_GLOBAL_DIR"
+npm config set prefix "$NPM_GLOBAL_DIR"
+
+export PATH="$NPM_GLOBAL_DIR/bin:$PATH"
 
 npm i -g n
 n lts
