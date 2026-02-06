49251317b3f090c0e3627b34d43a60547b57bc1b
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2026-02-06 16:53:20 -0300

    Crea dir para n

--- scripts/07-node.sh
+++ scripts/07-node.sh
@@ -3,12 +3,11 @@ set -euo pipefail
 
 sudo pacman -S --noconfirm --needed nodejs 
 
-export NPM_GLOBAL_DIR="$HOME/.npm-global"
-
 mkdir -p "$NPM_GLOBAL_DIR"
+mkdir -p "$N_PREFIX"
+
 npm config set prefix "$NPM_GLOBAL_DIR"
 
-export PATH="$NPM_GLOBAL_DIR/bin:$PATH"
 
 npm i -g n
 n lts
